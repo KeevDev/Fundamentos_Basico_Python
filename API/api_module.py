0ad04e14a93f5e78bb5e101b2922874482cf5b86
Kevin S <serna0667@gmail.com>
2023-08-30 18:53:56 -0500

    ensayo

--- API/api_module.py
+++ API/api_module.py
@@ -1,2 +1,6 @@
 def lo_logre():
     print("aqui estoy conectado")
+
+class punto:
+    def __init__(self):
+        print("INIT")   
--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,2 @@
-from api.api_module import lo_logre
-import sys
-sys.path.append('C:/Users/angie/OneDrive/EscritorioProyecto_ProgramacionIV')
-
-
-
+from api.api_module import *
 lo_logre()
