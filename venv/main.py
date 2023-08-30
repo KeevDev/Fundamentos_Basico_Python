c1c7b38882974c1c7a17aca78c43441afd121875
Kevin S <serna0667@gmail.com>
2023-08-30 17:01:45 -0500

    update of folder names and conection main with api

--- venv/main.py
+++ venv/main.py
@@ -1,10 +1,10 @@
-from api.__init__ import *
+from api import *
 
-def main():
-    print("DIGITE SU\n")
-    print("Departamento:")
-    print("Municipio:")
-    print("Cultivo:")
+# def main():
+#     print("DIGITE SU\n")
+#     print("Departamento:")
+#     print("Municipio:")
+#     print("Cultivo:")
 
 lo_logre()
 
